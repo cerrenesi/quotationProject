f3a8cf93cec48bf5b2e26bc298e7d3b839e609d6
cerrenesi <cerrenesi@gmail.com>
2020-01-25 17:20:01 +0300

    init commit

---
+++ src/main/java/ru/quotationProject/QuotationProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.quotationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/quotationProject/quotationProject/QuotationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.quotationProject.quotationProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
